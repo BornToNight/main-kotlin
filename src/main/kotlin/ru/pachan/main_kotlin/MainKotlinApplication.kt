2f8b8ae6bc00febba9837f7399d31c4080226be6
Vadim <isako-vadim@yandex.ru>
2025-10-30 16:34:30 +0500

    metrics

--- src/main/kotlin/ru/pachan/main_kotlin/MainKotlinApplication.kt
+++ src/main/kotlin/ru/pachan/main_kotlin/MainKotlinApplication.kt
@@ -1,9 +1,17 @@
 package ru.pachan.main_kotlin
 
+import net.devh.boot.grpc.client.autoconfigure.GrpcClientMetricAutoConfiguration
+import net.devh.boot.grpc.client.autoconfigure.GrpcClientMicrometerTraceAutoConfiguration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
+// EXPLAIN_V Ломает метрики
+@EnableAutoConfiguration(exclude = [
+    GrpcClientMetricAutoConfiguration::class,
+    GrpcClientMicrometerTraceAutoConfiguration::class
+])
 @EnableCaching
 @SpringBootApplication
 class MainKotlinApplication
